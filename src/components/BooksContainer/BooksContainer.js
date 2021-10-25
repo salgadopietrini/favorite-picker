b8e9097a5c2f608e4d9c1470172c9a7d2af17681
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-10-25 19:07:41 +0100

    add grid

--- src/components/BooksContainer/BooksContainer.js
+++ src/components/BooksContainer/BooksContainer.js
@@ -5,6 +5,16 @@ import FavoriteBooks from "../FavoriteBooks/FavoriteBooks";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import NavBar from "../NavBar/NavBar";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function BooksContainer() {
   const [books, setBooks] = useState([]);
@@ -29,21 +39,29 @@ export default function BooksContainer() {
         <CssBaseline />
         <Container maxWidth="lg">
           {favorites.length > 0 && <FavoriteBooks list={favorites} />}
-          {books.length > 0 ? (
-            books.map((elem) => (
-              <Book
-                info={elem}
-                handleSelect={handleSelect}
-                key={Math.floor(Math.random() * 10000)}
-              />
-            ))
-          ) : (
-            <h1>Loading books...</h1>
-          )}
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {books.length > 0 ? (
+              books.map((elem) => (
+                <Grid
+                  item
+                  xs={2}
+                  sm={4}
+                  md={4}
+                  key={Math.floor(Math.random() * 10000)}
+                >
+                  <Book info={elem} handleSelect={handleSelect} />
+                </Grid>
+              ))
+            ) : (
+              <h1>Loading books...</h1>
+            )}
+          </Grid>
         </Container>
       </React.Fragment>
     </div>
   );
 }
-
-
