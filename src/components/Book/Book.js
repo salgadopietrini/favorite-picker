4531d1aca2ee8e545242353fbe7eaeade4af5253
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-10-25 21:16:42 +0100

    add link to twitter

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,32 +1,23 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import { height } from '@mui/system';
-
-
-
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import { height } from "@mui/system";
 
 export default function Book(props) {
-  
-
   const selectBook = () => {
     props.handleSelect(props.info);
   };
 
-  
-
-  
   return (
     <div>
-    
       <Card sx={{ maxWidth: 180 }}>
         <CardHeader
-          titleTypographyProps={{variant:'body1' }}
+          titleTypographyProps={{ variant: "body1" }}
           title={props.info.title}
           subheader={props.info.data}
         />
@@ -37,17 +28,28 @@ export default function Book(props) {
           image={props.info.image}
           alt="Book Cover"
         />
-      
-      
-      <CardActions disableSpacing>
-        <IconButton  aria-label="add to favorites">
-          <FavoriteIcon onClick={selectBook} />
-        </IconButton>
-        <IconButton  aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon onClick={selectBook} />
+          </IconButton>
+          <IconButton
+            aria-label="share"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "https://twitter.com/intent/tweet?text=" +
+                  "My favorite book is " +
+                  `${props.info.title}` +
+                  "!! <3333",
+                "_blank"
+              );
+            }}
+          >
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
     </div>
   );
 }
--- src/components/FavoriteBookPreview/FavoriteBookPreview.js
+++ src/components/FavoriteBookPreview/FavoriteBookPreview.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 export default function FavoriteBookPreview(props) {
   return (
     <div>
-      <img alt="Book Cover" src={props.info.image} width="100px"></img>
+      <Box sx={{ "& > :not(style)": { m: 1 } }}>
+        <img alt="Book Cover" src={props.info.image} width="100px"></img>
+        <Fab size="small" color="primary" aria-label="delete">
+          <HighlightOffIcon />
+        </Fab>
+      </Box>
     </div>
   );
 }
